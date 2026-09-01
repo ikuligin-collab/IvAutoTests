d70162f21fddbd07e0a5020131346a0fa0783687
Kuligin <i.kuligin@evernode.tech>
2026-09-01 20:22:51 +0200

    коммит 2

--- apitest/Interfaces/IUserApiClient.cs
+++ apitest/Interfaces/IUserApiClient.cs
@@ -1,6 +1,10 @@
-﻿namespace apitest.Interfaces;
+﻿using apitest.DTO;
+using Refit;
 
+namespace apitest.Interfaces;
+[Headers ("x-api-key", "free_user_3IMkASnOWZzNwN8EVQkJPchdUZQ")]
 public interface IUserApiClient
 {
-    
+    [Get ("/users/{id}")]
+    Task<UserResponseDTO> GetUserAsync(int id);
 }
---
+++ apitest/Tests/RefitTests.cs
@@ -0,0 +1,11 @@
+﻿using apitest.Interfaces;
+using apitest.DTO;
+using Refit;
+
+namespace apitest;
+
+public class RefitTests
+{
+    private IUserApiClient _client;
+   
+}

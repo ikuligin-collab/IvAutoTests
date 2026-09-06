3b70f1a938dcd19ba4535ed27afa81b1b05097c2
Kuligin <i.kuligin@evernode.tech>
2026-09-06 19:19:18 +0200

    середина 15 занятия

---
+++ apitest/Tests/OrderTests.cs
@@ -0,0 +1,16 @@
+﻿using apitest.DTO.FilesDTO;
+using System.Text.Json;
+
+namespace apitest;
+
+public class OrderTests
+{
+    private OrderDataDTO order;
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "Resources", "Orders.json");
+        string data = File.ReadAllText(path);
+        order = JsonSerializer.Deserialize<OrderDataDTO>(data);
+    }
+}

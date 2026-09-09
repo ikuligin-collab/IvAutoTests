33420370f194b69cfff2cfcd6605399111b2e546
Kuligin <i.kuligin@evernode.tech>
2026-09-09 10:16:47 +0200

    Исправление комментария к ДЗ 14/15

--- apitest/Tests/OrderTests.cs
+++ apitest/Tests/OrderTests.cs
@@ -56,12 +56,6 @@ public class OrderTests
     public void Test5()
     {
         var mostExpensiveItem = order.Items.OrderByDescending(x=>x.Price).First();
-       /* using (new AssertionScope())
-        {
-            mostExpensiveItem.Price.Should().Be(129.99m);
-            mostExpensiveItem.Name.Should().Be("Wireless Headphones"); 
-        }*/
-       //mostExpensiveItem.Price.Should().Be(129.99m).And. mostExpensiveItem.Name.Should().Be("Wireless Headphones");
       //Проверка нескольких полей через анонимные типы
        mostExpensiveItem.Should().BeEquivalentTo(new
        {

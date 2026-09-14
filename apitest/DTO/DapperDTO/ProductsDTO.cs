38a15a6459eb1328049fa1b0235e2bab6abf444c
Kuligin <i.kuligin@evernode.tech>
2026-09-14 16:47:53 +0200

    Сущности для таблицы продуктов

---
+++ apitest/DTO/DapperDTO/ProductsDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace apitest.DTO.DapperDTO;
+
+public class ProductsDTO
+{
+    public int Id { get; set; }
+    public int Name { get; set; }
+    public int Description { get; set; }
+    public int Price { get; set; }
+    public int Stock { get; set; }
+    public int CategoryId { get; set; }
+}
---
+++ apitest/DapperRepository/ProductsRepository.cs
@@ -0,0 +1,30 @@
+﻿using apitest.Interfaces.DapperInterface;
+using apitest.DTO.DapperDTO;
+using Dapper;
+using Microsoft.Data.Sqlite;
+
+namespace apitest.DapperRepository;
+
+public class ProductsRepository: IProductsRepository
+{
+    private readonly string connectionString;
+    
+    public ProductsRepository(string connection)
+    {
+        connectionString = connection;
+    }
+    
+    public async Task<IEnumerable<ProductsDTO>> GetAllProductsAsync()
+    {
+        using var db = new SqliteConnection(connectionString);
+        var products = await db.QueryAsync<ProductsDTO>("SELECT * FROM Products");
+        return products;
+    }
+
+    public async Task<ProductsDTO> GetProductByIdAsync(int id)
+    {
+        using var db = new SqliteConnection(connectionString);
+        var product = await db.QueryFirstOrDefaultAsync<ProductsDTO>("SELECT * FROM Products WHERE Id = @id", new { id });
+        return product;
+    }
+}
